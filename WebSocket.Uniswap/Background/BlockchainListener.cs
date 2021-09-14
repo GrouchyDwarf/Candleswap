621ea8347aedb5704f94232d6377500d9408a6ed
GrouchyDwarf <ivanyeremenko3@gmail.com>
2021-09-14 20:17:49 +0300

    removed oldCandles

--- WebSocket.Uniswap/Background/BlockchainListener.cs
+++ WebSocket.Uniswap/Background/BlockchainListener.cs
@@ -47,83 +47,19 @@ namespace WebSocket.Uniswap.Background
 
         private async Task DoWork(CancellationToken cancellationToken)
         {
-            //var lastBlockInBlockchain = await _web3.Eth.Blocks.GetBlockNumber.SendRequestAsync();
-            var startFrom = DateTime.UtcNow;
-
-            //var lastBlockNumberInBlockchain = await _logicService.GetBlockNumberByDateTimeAsync(false, startFrom);
             var lastBlockNumberInBlockchain = await _web3.Eth.Blocks.GetBlockNumber.SendRequestAsync();
 
             var connection = _sqlConnectionProvider.GetConnection();
 
-            Task.Run(async () =>
+            foreach (var candle in Logic2.newCandles(_web3, _logger, connection,
+                                                     lastBlockNumberInBlockchain))
             {
-
-                foreach(var candle in Logic2.newCandles(_web3, _logger, connection,
-                    lastBlockNumberInBlockchain))
-                {
-                    var pair = await _candleStorageService.FetchPairAsync(candle.pair.token0Id, candle.pair.token1Id);
-                    DbCandle dbCandle = new((long)candle.datetime, candle.resolution,
-                            pair.Value.id, candle._open.ToString(), candle.high.ToString(), candle.low.ToString(), candle.close.ToString(), (int)candle.volume);
-                    await _candleStorageService.AddCandleAsync(dbCandle);
-                    WebSocketConnection.OnCandleUpdateReceived((pair.Value, dbCandle));
-                }
-            });
-            
-            Task.Run(async () =>
-            {
-                foreach (var c in Logic2.oldCandles(_web3, _logger, connection, lastBlockNumberInBlockchain))
-                {
-                    var pair = await _candleStorageService.FetchPairAsync(c.pair.token0Id, c.pair.token1Id);
-                    DbCandle dbCandle = new((long)c.datetime, c.resolution,
-                        pair.Value.id, c._open.ToString(), c.high.ToString(), c.low.ToString(), c.close.ToString(), (int)c.volume);
-                    await _candleStorageService.AddCandleAsync(dbCandle);
-                }
-            });
-
-
-            //var pancakeLauchDateTimestamp = new DateTime(2020, 9, 20, 0, 0, 0);
-
-            //_indexerService.IndexInRangeParallel(lastBlockNumberInBlockchain.Value,
-            //                                     0,
-            //                                     FSharpOption<BigInteger>.None);
-
-            //_indexerService.IndexNewBlockAsync(5);
-
-            //foreach (var period in _defaultPeriods)
-            //    _logicService.GetCandle(WebSocketConnection.OnCandleUpdateReceived, TimeSpan.FromSeconds(period),
-            //        cancellationToken);
-
-            //foreach(var period in _defaultPeriods)
-            //{
-            //    _logicService.GetCandles(_ => { }, cancellationToken, 
-            //                             (startFrom, pancakeLauchDateTimestamp), TimeSpan.FromSeconds(period));
-            //}
-
-
+                var pair = await _candleStorageService.FetchPairAsync(candle.pair.token0Id, candle.pair.token1Id);
+                DbCandle dbCandle = new((long)candle.datetime, candle.resolution,
+                        pair.Value.id, candle._open.ToString(), candle.high.ToString(), candle.low.ToString(), candle.close.ToString(), (int)candle.volume);
+                WebSocketConnection.OnCandleUpdateReceived((pair.Value, dbCandle));
+            }
         }
-
-       /* private async Task OnCandlesUpdateGet(List<Logic2.Candle> candles)
-        {
-            foreach(var candle in candles)
-            {
-                var pair = await _candleStorageService.FetchPairAsync(candle.pair.token0Id, candle.pair.token1Id);
-                DbCandle dbCandle = new((long)candle.datetime, candle.resolution,
-                        pair.Value.id, candle._open.ToString(), candle.high.ToString(), candle.low.ToString(), candle.close.ToString(), (int)candle.volume);
-                WebSocketConnection.OnCandleUpdateReceived((pair.Value, dbCandle));
-            }
-        }*/
-        /*
-         private async Task OnCalculatedCandlesGet(IEnumerable<Logic2.Candle> candles)
-         {
-            foreach (var candle in candles)
-            {
-                var pair = await _candleStorageService.FetchPairAsync(candle.pair.token0Id, candle.pair.token1Id);
-                DbCandle dbCandle = new((long)candle.datetime, candle.resolution,
-                        pair.Value.id, candle._open.ToString(), candle.high.ToString(), candle.low.ToString(), candle.close.ToString(), (int)candle.volume);
-                await _candleStorageService.AddCandleAsync(dbCandle);
-                WebSocketConnection.OnCandleUpdateReceived((pair.Value, dbCandle));
-            }
-        }*/
 
     }
 }
